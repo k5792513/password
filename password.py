0e1ded5417391aa020f832b567398ff3f561eeb4
Nick Huang <k5792513@yahoo.com.tw>
2019-11-10 11:30:08 +0800

    while x > 0

--- password.py
+++ password.py
@@ -1,13 +1,15 @@
 
 password = 'a123456'
 x = 3
-while True:
+while x  > 0:
+	x = x - 1
 	pwd = input('請輸入密碼： ')
 	if pwd == password:
 		print('你已經成功登入')
 		break
 	else:
-		x = x - 1
-		print('密碼錯誤！你還有', x, '機會')
-		if x == 0:
-			break
+		print('密碼錯誤')
+		if x > 0:
+			print('還有', x, '次機會')
+		else:
+			print('帳號被鎖，請洽管理員')
